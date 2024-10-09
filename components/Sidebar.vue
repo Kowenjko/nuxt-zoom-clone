69fa0bbdf9982daa2fde337038aa95a6b3e687de
Kowenjko <ortoswt@gmail.com>
2024-10-09 15:33:01 +0300

    add sidebar

---
+++ components/Sidebar.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import { sidebarLinks } from '@/constants'
+
+const route = useRoute()
+
+console.log(route.path)
+</script>
+<template>
+	<section
+		class="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]"
+	>
+		<div class="flex flex-1 flex-col gap-6">
+			<NuxtLink
+				v-for="item in sidebarLinks"
+				:key="item.label"
+				:to="item.route"
+				class="flex gap-4 items-center p-4 rounded-lg justify-start"
+				:class="{ 'bg-blue-1': route.path === item.route }"
+			>
+				<img v-if="item.imgURL" :src="item.imgURL" :alt="item.label" width="24" height="24" />
+				<p class="text-lg font-semibold max-lg:hidden">{{ item.label }}</p>
+			</NuxtLink>
+		</div>
+	</section>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -9,10 +9,10 @@ useHead({
 </script>
 <template>
 	<main class="relative">
-		Navbar
+		<Navbar />
 
 		<div class="flex">
-			Sidebar
+			<Sidebar />
 
 			<section class="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
 				<div class="w-full"><slot /></div>
